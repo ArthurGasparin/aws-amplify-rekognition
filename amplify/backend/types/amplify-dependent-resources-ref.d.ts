ef6f5c23eb78f1f6613f362c19fa4b8f9af5c484
ArthurGasparin <arthursantori@gmail.com>
2023-10-27 11:03:53 -0300

    🎉 init(face liveness)

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "awsrekognitionlivene1efd1805": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
